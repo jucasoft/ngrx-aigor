fb478538d7a7dd13e5de504cefe5a8461953e80f
juca <juca@jucasoft.com>
2021-02-21 08:10:09 +0100

    "build": "npm run build:lib:prod && ng build --aot --prod",

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 
-const routes: Routes = [{path: '', redirectTo: 'home', pathMatch: 'full'}, {path: 'home', loadChildren: () => import('./main/views/home/home.module').then(m => m.HomeModule)}, {path: 'user', loadChildren: () => import('./main/views/user/user.module').then(m => m.UserModule)}, {path: 'address', loadChildren: () => import('./main/views/address/address.module').then(m => m.AddressModule)}, {path: 'company', loadChildren: () => import('./main/views/company/company.module').then(m => m.CompanyModule)},];
+const routes: Routes = [
+  {path: '', redirectTo: 'user', pathMatch: 'full'},
+  {path: 'home', loadChildren: () => import('./main/views/home/home.module').then(m => m.HomeModule)},
+  {path: 'user', loadChildren: () => import('./main/views/user/user.module').then(m => m.UserModule)},
+  {path: 'address', loadChildren: () => import('./main/views/address/address.module').then(m => m.AddressModule)},
+  {path: 'company', loadChildren: () => import('./main/views/company/company.module').then(m => m.CompanyModule)},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: false, relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, {enableTracing: false, relativeLinkResolution: 'legacy'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
