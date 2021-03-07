24d5cecc703e08fa7cdd9446e590d1c9c57a95a0
juca <juca@jucasoft.com>
2021-03-07 13:50:59 +0100

    versione stabile funzionante.

--- projects/ngrx-aigor/src/lib/ngrx-aigor.component.ts
+++ projects/ngrx-aigor/src/lib/ngrx-aigor.component.ts
@@ -6,7 +6,6 @@ import {Component, ViewEncapsulation} from '@angular/core';
     <lib-ngrx-aigor-container></lib-ngrx-aigor-container>
   `,
   styles: [],
-  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class NgrxAigorComponent {
 
